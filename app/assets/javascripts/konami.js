72f1baddd09051812ec6a05651fc69f952bdc988
Travis Cowx <tcowx21@gmail.com>
2018-01-31 10:47:01 -0500

    Revert "Yeah"
    This reverts commit 9c89b354f11bd8ef0d79bd57ff218ce24aea86db.

--- app/assets/javascripts/konami.js
+++ app/assets/javascripts/konami.js
@@ -15,9 +15,6 @@ var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 
 // Remembers where the user has reached
 var konamiPosition = 0;
 
-// Number of times somebody has pressed the konami code on the page
-var konamiTimes = 0;
-
 // Keydown event listener
 document.addEventListener('keydown', function(e) {
     var inKey = e.which || e.keycode;
@@ -34,13 +31,6 @@ document.addEventListener('keydown', function(e) {
         // If the last key is reached play the audio
         if (konamiPosition == konamiCode.length) {
             alert('Konami code actived!');
-            konamiTimes++;  // Increment the tiems by one
-            // MAKE IMAGES AND MAKE IT MOVE MOVE
-            /*
-            for(var i=0;i<konamiTimes;i++) {
-                addMovingTurkey(i);
-            }
-            */
             // Contra title theme declaration
             var aud = document.getElementById('konami');
             aud.play();
@@ -52,26 +42,3 @@ document.addEventListener('keydown', function(e) {
     }
 
 }, true);
-
-// Adds turkey image to the screen, randomly locates it and
-// moves it accross the screen right to left. Deletes on reaching end
-function addMovingTurkey(number) {
-    // Add image
-    var newDiv = "turkey-" + number;     // Name of the turkey to move
-    var newEl = document.createElement('img');      // Create the element to be added
-    var parEl = document.getElementById('content');
-    // Get screen X & Y
-    var screenWidth = window.innerWidth;
-    var screenHeight = window.innerHeight;
-
-    var turkeyYPos = Math.random() * (screenHeight - 1) + 1;    // Get a random screen height
-    newEl.setAttribute("class", "turkey");      // Set the class for the image
-    newEl.style.top = turkeyYPos +"px";         // Set the Y Position on the screen
-
-    document.body.appendChild(newEl);
-
-    // Move image
-
-    // Delete image
-
-}
